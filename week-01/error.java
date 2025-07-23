8b1a01606edca9ad468cd209cc227f9927729e17
vaibhavgogo <vaibhavsmartkid@gmail.com>
2025-07-24 01:07:05 +0530

    commit

---
+++ week-01/error.java
@@ -0,0 +1,15 @@
+public class error {
+
+    public static void main(String[] args) {
+
+        // Using print()
+        System.err.print("This is an error message using print().\n");
+
+        // Using println()
+        System.err.println("This is another error message using println().");
+
+        // Using printf()
+        System.err.printf("Error code: %d, Message: %s%n", 404, "Not Found");
+    }
+
+}
---
+++ week-01/loop.java
@@ -0,0 +1,14 @@
+
+// Java program to demonstrates 
+// the working of do-while loop
+import java.io.*;
+
+class loop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 5);
+    }
+}
