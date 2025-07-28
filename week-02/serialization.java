6d7af4d69490914384ad8660b105923096d2c097
vaibhavgogo <vaibhavsmartkid@gmail.com>
2025-07-28 19:53:16 +0530

    commit

---
+++ week-02/serialization.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: " + name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Serialization
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("student.ser"))) {
+            out.writeObject(new Student("Alice"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialization
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("student.ser"))) {
+            Student s = (Student) in.readObject();
+            System.out.println(s); // Output: Student: Alice
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
